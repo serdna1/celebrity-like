b60ede14e408445bc60c73db44fa3bac7b522fed
Andrés Campaña Otero <andrescaot@gmail.com>
2023-03-26 13:13:37 +0200

    Hacer que aparezca la información relativa a una cara cuando se pasa el cursor sobre ella

--- src/components/Faces.jsx
+++ src/components/Faces.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useState, useContext } from 'react'
 
 import { FacesContext } from '../context/faces'
 
@@ -6,11 +6,29 @@ import './Faces.css'
 
 export const Faces = () => {
   const { faces } = useContext(FacesContext)
+  const [displayArticle, setDisplayArticle] = useState({})
+  // const [coords, setCoords] = useState({})
+
+  const handleOnMouseEnter = (i) => {
+    setDisplayArticle(old => ({ ...old, [i]: true }))
+  }
+
+  const handleOnMouseLeave = (i) => {
+    setDisplayArticle(old => ({ ...old, [i]: false }))
+  }
+
+  // const handleOnMouseOver = (event) => {
+  //   setCoords({
+  //     x: event.clientX,
+  //     y: event.clientY
+  //   })
+  // }
 
   return (
     <>
+      {/* <p style={{ color: '#000' }}>x: {coords.x}, y: {coords.y}</p> */}
       {
-        faces.map(face => (
+        faces.map((face, i) => (
           <div
             key={face.key}
             className='contour'
@@ -22,7 +40,24 @@ export const Faces = () => {
                       top: `${face.boundingBox.top * 100}%`
                     }
                   }
-          />
+            onMouseEnter={() => handleOnMouseEnter(i)}
+            onMouseLeave={() => handleOnMouseLeave(i)}
+            // onMouseMove={handleOnMouseOver}
+          >
+            <article
+              className='faceData'
+              style={
+                      {
+                        display: displayArticle[i] ? 'block' : 'none',
+                        top: '105%'
+                      }
+                    }
+            >
+              <p>Name: {face.name ? face.name : 'not famous'}</p>
+              <p>Emotion: {face.emotionType}</p>
+              {/* Aqui la data del famoso (si lo es) */}
+            </article>
+          </div>
         ))
       }
     </>
