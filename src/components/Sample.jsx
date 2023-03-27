738afc64e3b3947ac8e98691ba725e00e9afba33
Andrés Campaña Otero <andrescaot@gmail.com>
2023-03-27 19:19:28 +0200

    Change transformations of the sample images

--- src/components/Sample.jsx
+++ src/components/Sample.jsx
@@ -6,7 +6,7 @@ export const Sample = ({ filename, alt }) => {
   return (
     <a href={`https://res.cloudinary.com/${cloudName}/image/upload/v1678100497/celebrity-like/samples/${filename}`}>
       <div className='sample'>
-        <img src={`https://res.cloudinary.com/${cloudName}/image/upload/c_thumb,g_auto:face,h_150,w_150,z_0.7/f_webp/q_auto:low/celebrity-like/samples/${filename}`} alt={`${alt}`} />
+        <img src={`https://res.cloudinary.com/${cloudName}/image/upload/c_thumb,g_auto:faces,h_150,w_150,z_0.6/f_webp/q_auto/celebrity-like/samples/${filename}`} alt={`${alt}`} />
       </div>
     </a>
   )
