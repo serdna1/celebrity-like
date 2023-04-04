2e34ea28b5d39627dbafdbccea7c34181a57afda
Andrés Campaña Otero <andrescaot@gmail.com>
2023-04-04 21:23:16 +0200

    BugFix: Face data articles not displaying correctly sometimes on mobile

--- src/components/FixedFooter.jsx
+++ src/components/FixedFooter.jsx
@@ -1,14 +1,28 @@
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { FacesContext } from '../context/faces'
 
 import './FixedFooter.css'
 
 export const FixedFooter = () => {
-  const { loading } = useContext(FacesContext)
+  // const { loading } = useContext(FacesContext)
+  const [innerWidth, setInnerWidth] = useState(null)
+
+  useEffect(() => {
+    const onResize = () => {
+      setInnerWidth(window.innerWidth)
+    }
+
+    window.addEventListener('resize', onResize)
+
+    return () => {
+      window.removeEventListener('resize', onResize)
+    }
+  }, [])
 
   return (
     <footer className='footer'>
-      loading: {`${loading}`}
+      {/* loading: {`${loading}`} */}
+      window.innerWidth: {innerWidth}
     </footer>
   )
 }
