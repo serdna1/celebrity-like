13d67e0eecd345bf12a13cf8051ada201d6fb5d1
Andrés Campaña Otero <andrescaot@gmail.com>
2023-04-05 12:41:21 +0200

    Add filepond plugins

--- src/components/CustomFilePond.jsx
+++ src/components/CustomFilePond.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
-import { FilePond } from 'react-filepond'
+import { FilePond, registerPlugin } from 'react-filepond'
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
 
 import { useFilePondServer } from '../hooks/useFilePondServer'
 
 import 'filepond/dist/filepond.min.css'
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
 
 import './CustomFilePond.css'
+
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview)
 
 export const CustomFilePond = () => {
   const [files, setFiles] = useState([])
